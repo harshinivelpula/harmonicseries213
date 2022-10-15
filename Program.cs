5586929737bc28b45a78db66fb517da7be41c9dc
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-15 19:42:59 +0530

    checkseries

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+namespace Harmonic
+{
+    public static  class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, num;
+            Console.WriteLine("Number of input: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= num; i++)
+            {
+                Console.WriteLine("1/{0} + ", i);
+               
+                Console.WriteLine("nth  Harmonic Number : 1/{0} ", num);
+            }
+
+        }
+    }
+}
